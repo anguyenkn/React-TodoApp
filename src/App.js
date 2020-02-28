8e3721f0cacceb833e61500f65ec7856c5799e4b
Aidan Nguyen <anguyenkn@gmail.com>
2020-02-28 12:40:17 -0800

    convert functional component to class components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import TodoItem from "./components/TodoItem"
 import data from "./todosData"
 
-function App() {
-  const items_rendered = data.map(item => <TodoItem key={item.id} info={item}/>);
-
-  return (
-    <div className="todo-list">
-      {items_rendered}
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    const items_rendered = data.map(item => <TodoItem key={item.id} info={item}/>);
+    return (
+      <div className="todo-list">
+        {items_rendered}
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,17 @@
 import React from "react"
 
-function TodoItem(props){
-    let checkbox_render = props.info.completed == true ? 
-    <input type="checkbox"/> : <input type="checkbox" checked="checked"/>
+class TodoItem extends React.Component{
+    render(){
+        let checkbox_render = this.props.info.completed == true ? 
+        <input type="checkbox"/> : <input type="checkbox" checked="checked"/>
 
-    return(
-        <div className="todo-item">
-            {checkbox_render}
-            <label name={props.info.text}> {props.info.text}</label><br />
-        </div>
-    );
+        return(
+            <div className="todo-item">
+                {checkbox_render}
+                <label name={this.props.info.text}> {this.props.info.text}</label><br />
+            </div>
+        );
+    }
 }
 
 export default TodoItem
