e4f9575374332eede114c729b5cc1e62e4eae8da
Aidan Nguyen <anguyenkn@gmail.com>
2020-03-01 14:50:45 -0800

    minor updates

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,21 +2,20 @@ import React from "react"
 
 class TodoItem extends React.Component{
     render(){
-        const todoItemStyle = {}
-        if (this.props.item.completed) {
-            todoItemStyle["textDecoration"] = "line-through";
-            todoItemStyle["color"] = "gray";
-            todoItemStyle["fontStyle"] = "italic"
+        const todoItemStyle = {
+            textDecoration: "line-through",
+            color: "gray",
+            fontStyle: "italic"
         }
 
         return(
-            <div style={todoItemStyle} className="todo-item">
+            <div className="todo-item">
                 <input 
                     onChange={() => this.props.handleChange(this.props.item.id)} 
                     type="checkbox" 
                     checked={this.props.item.completed}
                 />
-                <label name={this.props.item.text}> {this.props.item.text}</label><br />
+                <label style={this.props.item.completed ? todoItemStyle :null} name={this.props.item.text}> {this.props.item.text}</label><br />
             </div>
         );
     }
